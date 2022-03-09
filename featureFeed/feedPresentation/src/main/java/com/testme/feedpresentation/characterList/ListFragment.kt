2adbb495396fe39a3e1d204091b1eb271c3eed02
Juan Pablo Scigliano <juanscigliano@gmail.com>
2022-03-09 13:40:54 +0100

    [Feature - Presentation] Add basic stuff for Presentation module

---
+++ featureFeed/feedPresentation/src/main/java/com/testme/feedpresentation/characterList/ListFragment.kt
@@ -0,0 +1,9 @@
+package com.testme.feedpresentation.characterList
+
+import androidx.fragment.app.Fragment
+import com.testme.feedpresentation.R
+
+
+class ListFragment : Fragment(R.layout.fragment_list) {
+
+}
