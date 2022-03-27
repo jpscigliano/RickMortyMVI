ca572a0b4935d2528600309db9ba6baefedbca4c
Juan Pablo Scigliano <juanscigliano@gmail.com>
2022-03-27 15:48:35 +0200

    Clean unused classes

--- app/src/main/java/com/testme/rickandmoryfeed/MainActivity.kt
+++ app/src/main/java/com/testme/rickandmoryfeed/MainActivity.kt
@@ -2,9 +2,7 @@ package com.testme.rickandmoryfeed
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.testme.feedpresentation.HelloWorld
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
